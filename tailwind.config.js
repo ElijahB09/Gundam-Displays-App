ddfccbc4daee69f1d56f15a025b8f7b2650afb84
Elijah Brady <bradyeli09@gmail.com>
2023-08-16 00:08:31 -0500

    Honestly tailwind is giving me the business, this is so annoying

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,html}"],
+  content: ["./index.html", "./src/**/*.{js,html}", "./node_modules/flowbite/**/*.js", "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tw-elements/dist/plugin.cjs')
+  ],
 }
 
