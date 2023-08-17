95fef04dba5274e951aa82cf10208b4d79830121
Elijah Brady <bradyeli09@gmail.com>
2023-08-17 14:41:31 -0500

    Got tailwind working again, had to ditch tw-elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,html}", "./node_modules/flowbite/**/*.js", "./node_modules/tw-elements/dist/js/**/*.js"],
+  content: ["./index.html", "./src/**/*.{js,html}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-    require('tw-elements/dist/plugin.cjs')
-  ],
+  plugins: [],
 }
 
