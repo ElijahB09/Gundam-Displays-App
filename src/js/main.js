ee7356b49c1b20b5f524708ef5c3c606c43cf006
Elijah Brady <bradyeli09@gmail.com>
2023-08-17 15:40:51 -0500

    extending out into js files cause I'm tired of staring at css

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,18 @@
-function turnLedOn() {
-    
+function toggleLed(toggle, gundam_name) {
+    switch (toggle) {
+        case "on":
+            ledOn(gundam_name);
+            break;
+        case "off":
+            ledOff(gundam_name);
+            break;
+    }
 }
 
-function turnLedOff() {
+function ledOn(gundam_name) {
+
+}
+
+function ledOff(gundam_name) {
 
 }
