fb78663360905640cb17e10a5a0c232cc5935817
Elijah Brady <bradyeli09@gmail.com>
2023-08-21 10:28:36 -0500

    Adjustments to both services

--- src/js/services/uc-services/f91-service.js
+++ src/js/services/uc-services/f91-service.js
@@ -1,3 +1,9 @@
 export function f91Toggle(operation) {
     // This will make call to the backend to toggle the f91 lights
+    switch(operation) {
+        case "on":
+            break;
+        case "off":
+            break;
+    }
 }
--- src/js/services/uc-services/unicorn-service.js
+++ src/js/services/uc-services/unicorn-service.js
@@ -1,3 +1,9 @@
 export function unicornToggle(operation) {
     // Call backend to toggle unicorn leds
+    switch(operation) {
+        case "on":
+            break;
+        case "off":
+            break;
+    }
 }
