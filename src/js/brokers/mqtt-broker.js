a2f53f131c26ba00196cc3f9e134a2862c8a2ebd
Elijah Brady <bradyeli09@gmail.com>
2023-08-30 22:47:33 -0500

    Reconfigured so that the subscriptions would execute on startup

--- src/js/brokers/mqtt-broker.js
+++ src/js/brokers/mqtt-broker.js
@@ -4,48 +4,41 @@ const body = document.getElementById("body");
 body.addEventListener("load", subClientsToBroker(), false);
 
 function subClientsToBroker() {
+  const mqtt = require('mqtt')
 
-}
+  //EMQX's default port for mqtt connections is 1883, while for mqtts it is 8883.
+  const url = `mqtt://${process.env.PI_MQTT_BROKER}:1883`
 
-const mqtt = require('mqtt')
+  const clientsToBuild = [
+    {name: "f91", topic: "gundam/uc/f91/f91gundam"},
+    {name: "unicorn", topic: "gundam/uc/unicorn/unicorngundam"}
+  ]
 
-
-/***
-    * Node.js
-    * This document explains how to use MQTT over TCP with both mqtt and mqtts protocols.
-    * EMQX's default port for mqtt connections is 1883, while for mqtts it is 8883.
-    */
-const url = `mqtt://${process.env.PI_MQTT_BROKER}:1883`
-
-const clientsToBuild = [
-  {name: "f91", topic: "gundam/uc/f91/f91gundam"},
-  {name: "unicorn", topic: "gundam/uc/unicorn/unicorngundam"}
-]
-
-const secretMapping = {
-  f91: "GUNDAM_F91_PASSWORD",
-  unicorn: "GUNDAM_UNICORN_PASSWORD",
-}
-
-clientsToBuild.forEach((item) => {
-  var secretKey = secretMapping[item.name]
-  const options = {
-    clean: true,
-    connectTimeout: 4000,
-    clientId: 'mqtt_client_frontend_' + item.name,
-    username: item.name,
-    password: process.env[secretKey],
+  const secretMapping = {
+    f91: "GUNDAM_F91_PASSWORD",
+    unicorn: "GUNDAM_UNICORN_PASSWORD",
   }
-  const client  = mqtt.connect(url, options)
-  client.on('connect', function () {
-    console.log('Connected')
-    client.subscribe(`${item.topic}`, function (err) {
-      if (!err) {
-        client.publish(`${item.topic}`, 'activate')
-      }
+
+  clientsToBuild.forEach((item) => {
+    var secretKey = secretMapping[item.name]
+    const options = {
+      clean: true,
+      connectTimeout: 4000,
+      clientId: 'mqtt_client_frontend_' + item.name,
+      username: item.name,
+      password: process.env[secretKey],
+    }
+    const client  = mqtt.connect(url, options)
+    client.on('connect', function () {
+      console.log('Connected')
+      client.subscribe(`${item.topic}`, function (err) {
+        if (!err) {
+          client.publish(`${item.topic}`, 'activate')
+        }
+      })
     })
-  })
-});
+  });
+}
 
 // Receive messages
 client.on('message', function (topic, message) {
