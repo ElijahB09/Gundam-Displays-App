56c6491419612bda5d81867ca5410272a992d5c9
Elijah Brady <bradyeli09@gmail.com>
2023-09-09 13:26:22 -0500

    I have done a ton, still trying to get a connection up

--- src/js/brokers/mqtt-broker.js
+++ src/js/brokers/mqtt-broker.js
@@ -1,32 +1,31 @@
-require('dotenv').config();
+import * as mqtt from 'mqtt/dist/mqtt.min'
 
-const body = document.getElementById("body");
-body.addEventListener("load", subClientsToBroker(), false);
+const body = document.getElementById('body');
+body.addEventListener('load', subClientsToBroker(), false);
 
 function subClientsToBroker() {
-  const mqtt = require('mqtt')
-
   //EMQX's default port for mqtt connections is 1883, while for mqtts it is 8883.
-  const url = `mqtt://${process.env.PI_MQTT_BROKER}:1883`
+  const url = `mqtt://${import.meta.env.VITE_PI_MQTT_BROKER}:1883`
 
   const clientsToBuild = [
     {name: "f91", topic: "gundam/uc/f91/f91gundam"},
     {name: "unicorn", topic: "gundam/uc/unicorn/unicorngundam"}
   ]
 
-  const secretMapping = {
-    f91: "GUNDAM_F91_PASSWORD",
-    unicorn: "GUNDAM_UNICORN_PASSWORD",
-  }
-
   clientsToBuild.forEach((item) => {
-    var secretKey = secretMapping[item.name]
+    var user_password = null;
+    if (item.name == 'f91') {
+      user_password = import.meta.env.VITE_GUNDAM_F91_PASSWORD;
+    } else {
+      user_password = import.meta.env.VITE_GUNDAM_UNICORN_PASSWORD;
+    }
+
     const options = {
       clean: true,
       connectTimeout: 4000,
       clientId: 'mqtt_client_' + item.name + '_frontend',
       username: item.name + '_frontend',
-      password: process.env[secretKey],
+      password: user_password,
     }
     const client  = mqtt.connect(url, options)
     client.on('connect', function () {
